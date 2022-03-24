44614c2701a62ae650ac2c6da27352ab9c2038dc
lelek337 <80050352+lelek337@users.noreply.github.com>
2022-03-24 21:46:28 +0300

    init: start task1

--- my-new-app/src/app/app.module.ts
+++ my-new-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchItemComponent } from './search-item/search-item.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavebarComponent } from './navebar/navebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    ProfileComponent,
+    NavebarComponent
   ],
   imports: [
     BrowserModule,
